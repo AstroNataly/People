71d3e202d67cf8349e5c8b5b2189b320a981f87b
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-07-13 23:52:43 +0300

    modified:   src/main/kotlin/main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
+package ru.netology
+
 fun main() {
-    
+    val likes = 351
+
+    if ((likes % 10) == 1) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
 }
